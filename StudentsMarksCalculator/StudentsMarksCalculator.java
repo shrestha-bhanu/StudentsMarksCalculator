e30766c6fe9e5a65a82920f7bf4a20f4190ee997
shrestha-bhanu <bhanupratapshrestha@gmail.com>
2024-08-01 20:10:39 +1000

    Created the main function, and added menu, and switch cases to handle the menu options.

--- StudentsMarksCalculator/StudentsMarksCalculator.java
+++ StudentsMarksCalculator/StudentsMarksCalculator.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class StudentsMarksCalculator here.
  *
@@ -7,16 +7,47 @@
  */
 public class StudentsMarksCalculator
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
     /**
      * Constructor for objects of class StudentsMarksCalculator
      */
-    public StudentsMarksCalculator()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = "";
+        
+        while (true) {
+            System.out.println("Student Marks Calculator");
+            System.out.println("1. Read input from file");
+            System.out.println("2. Print student marks");
+            System.out.println("3. Print marks below threshold");
+            System.out.println("4. Sort marks and filter students");
+            System.out.println("5. Exit");
+            System.out.println("Enter task (1-5)");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the file name: ");
+                    fileName = scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.println("case 2");
+                    break;
+                case 3:
+                    System.out.println("case 3");
+                    break;
+                case 4:
+                    System.out.println("case 4");
+                    break;
+                case 5:
+                    System.out.println("Exiting the program. Thank you");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid task. Please try again.");
+            }
+        }
     }
 
     /**
@@ -25,9 +56,5 @@ public class StudentsMarksCalculator
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+ 
 }
